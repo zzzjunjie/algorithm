86f0128e9f4269599abb66f2d2735aded0cc6c87
Pangu <1367970137@qq.com>
2020-07-15 23:26:08 +0800

    归并排序

--- src/排序算法/归并排序.java
+++ src/排序算法/归并排序.java
@@ -10,21 +10,21 @@ public class 归并排序 {
   public static void main(String[] args) {
     int[] a = {1,10,8,9,2,6,4};
 //    MergeSort(a);
-    int mid = a.length>>1;
-    Merge(a,0,mid,a.length-1);
+    Merge(a,0,a.length-1);
     System.out.println(Arrays.toString(a));
   }
 
-  public static void Merge(int[] array, int low, int mid, int high) {
+  public static void Merge(int[] array, int low, int high) {
     if(low>=high)return;
+    int mid = (low+high)>>1;
     int i=low;//第一段扫描下标
     int j=mid+1;//第二段扫描下标
     int[] array2 = new int[high-low+1];//临时数组
     int k=0;//临时数组下标
     // 扫描第一段和第二段序列，直到有一个扫描结束
 
-    Merge(array,i,(i+mid)>>1,mid);
-    Merge(array,j,(j+high)>>1,high);
+    Merge(array,i,mid);
+    Merge(array,j,high);
 
     while (i<=mid && j <=high){
       // 判断第一段和第二段取出的数哪个更小，将其存入合并序列，并继续向下扫描

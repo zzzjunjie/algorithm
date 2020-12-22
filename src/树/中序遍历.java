882d9284849ad95d5223cd2dd636275995807f6c
zhoujunjie <zhoujunjie@xingfeiinc.com>
2020-12-22 21:21:59 +0800

    新增中序遍历排序树

---
+++ src/树/中序遍历.java
@@ -0,0 +1,27 @@
+package 树;
+
+import 树.节点.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 中序遍历 {
+
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> vals = new ArrayList();
+        inorder(root, vals);
+        TreeNode ans = new TreeNode(0), cur = ans;
+        for (int v: vals) {
+            cur.right = new TreeNode(v);
+            cur = cur.right;
+        }
+        return ans.right;
+    }
+
+    public void inorder(TreeNode node, List<Integer> vals) {
+        if (node == null) return;
+        inorder(node.left, vals);
+        vals.add(node.val);
+        inorder(node.right, vals);
+    }
+}
--- src/树/节点/TreeNode.java
+++ src/树/节点/TreeNode.java
@@ -5,5 +5,10 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
-    TreeNode(int x) { val = x; }
+    public TreeNode(int x) { val = x; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }

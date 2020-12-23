ab7333e9d14f72acd816679410ae4f7645526e8c
zhoujunjie <zhoujunjie@xingfeiinc.com>
2020-12-23 09:32:03 +0800

    添加注释

--- src/树/平衡二叉树.java
+++ src/树/平衡二叉树.java
@@ -16,14 +16,17 @@ public class 平衡二叉树 {
         if (root==null) {
             return 0;
         }
+        //先计算左边树的深度
         int left = recur(root.left);
         if (left==-1) {
             return -1;
         }
+        // 计算右边树的深度
         int right = recur(root.right);
         if (right==-1) {
             return -1;
         }
+        // 左右两边深度做相差大于1就返回-1 否则返回他们的最大深度
         return Math.abs(left-right) <2?Math.max(left,right)+1:-1;
     }
 }

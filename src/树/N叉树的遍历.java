e32559e888aefac26aba708d3961c02697d0711d
zhoujunjie <1367970137@qq.com>
2020-12-29 23:47:27 +0800

    新增N叉树的遍历

--- src/树/N叉树的遍历.java
+++ src/树/N叉树的遍历.java
@@ -3,15 +3,5 @@ package 树;
 import 树.节点.Node;
 
 public class N叉树的遍历 {
-    public static void func(Node root){
-        if (root==null){
-            return ;
-        }
-        // 先序遍历
-        System.out.println(root.val);
-        for (Node child : root.children) {
-            func(child);
-        }
-        // 后序遍历
-    }
+
 }

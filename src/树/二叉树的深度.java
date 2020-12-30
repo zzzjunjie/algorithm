977f9e26216b4fa37406fbfa203783b104eb50f5
zhoujunjie <zhoujunjie@xingfeiinc.com>
2020-12-30 15:01:56 +0800

    新增DFS遍历方式

--- src/树/二叉树的深度.java
+++ src/树/二叉树的深度.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class 二叉树的深度 {
+    // 解法1：使用BFS进行遍历
     public int maxDepth(TreeNode root) {
         // 根节点为null没有深度
         if (root==null){
@@ -37,4 +38,16 @@ public class 二叉树的深度 {
         return dep;
     }
 
+    // 解法2：使用DFS进行遍历
+    int maxDep = 0;
+    public int maxDepth2(TreeNode root){
+        if (root==null){
+            return  maxDep;
+        }
+        maxDep++;
+        int leftDep = maxDepth2(root.left);
+        maxDep = 0;
+        int rightDep = maxDepth2(root.right);
+        return Math.max(leftDep,rightDep);
+    }
 }

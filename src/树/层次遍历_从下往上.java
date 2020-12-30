2874547098e53a6be4bd2eb0d3a56ccaa95955ec
zhoujunjie <zhoujunjie@xingfeiinc.com>
2020-12-30 21:24:44 +0800

    新增树的层次遍历（从下往上）

---
+++ src/树/层次遍历_从下往上.java
@@ -0,0 +1,37 @@
+package 树;
+
+import 树.节点.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 层次遍历_从下往上 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root==null){
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            List<Integer> val = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i <size; i++) {
+                TreeNode poll = queue.poll();
+                val.add(poll.val);
+                if (poll.left!=null){
+                    queue.offer(poll.left);
+                }
+                if (poll.right!=null){
+                    queue.offer(poll.right);
+                }
+            }
+            res.add(0,val);
+        }
+        return res;
+    }
+}

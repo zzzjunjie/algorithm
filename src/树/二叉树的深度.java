43da43a472632175d01661966da9443ffe389804
zhoujunjie <zhoujunjie@xingfeiinc.com>
2020-12-30 15:34:14 +0800

    删除无用参数

--- src/树/二叉树的深度.java
+++ src/树/二叉树的深度.java
@@ -39,7 +39,6 @@ public class 二叉树的深度 {
     }
 
     // 解法2：使用DFS进行遍历
-    int maxDep = 0;
     public int maxDepth2(TreeNode root) {
         if(root == null) return 0;
         return Math.max(maxDepth2(root.left), maxDepth2(root.right)) + 1;

4debe5a50527811dbbf3387cbe85ec7402affb14
zhoujunjie <1367970137@qq.com>
2021-01-01 09:16:52 +0800

    单值二叉树

---
+++ src/树/单值二叉树.java
@@ -0,0 +1,20 @@
+package 树;
+
+import 树.节点.TreeNode;
+
+public class 单值二叉树 {
+    int val = -1;
+    public boolean isUnivalTree(TreeNode root) {
+        if (root==null){
+            return true;
+        }
+        if(val==-1){
+            val = root.val;
+        }else{
+            if (root.val!=val){
+                return false;
+            }
+        }
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
+}

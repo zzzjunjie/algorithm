75f98fb7b74ef951d85d1842c2403d956a8a6bee
zhoujunjie <zhoujunjie@xingfeiinc.com>
2021-01-05 09:45:58 +0800

    二叉树中第二小的节点

---
+++ src/树/二叉树中第二小的借点.java
@@ -0,0 +1,37 @@
+package 树;
+
+import 树.节点.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 二叉树中第二小的借点 {
+    Queue<Integer> queue = new LinkedList<>();
+    public int findSecondMinimumValue(TreeNode root) {
+        dfs(root);
+        System.out.println(queue);
+        int first = queue.poll();
+        for (int i = 0; i < queue.size(); i++) {
+            Integer poll = queue.poll();
+            if (poll!=first){
+                return poll;
+            }
+        }
+        return -1;
+    }
+
+    public void dfs(TreeNode root){
+        if (root==null) {
+            return;
+        }
+        dfs(root.left);
+        queue.offer(root.val);
+        dfs(root.right);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

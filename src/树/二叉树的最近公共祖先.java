3292742d443fe2b23592309b6004d3ccca5bafe1
zhoujunjie <1367970137@qq.com>
2021-01-06 21:25:35 +0800

    二叉树的最近公共祖先

---
+++ src/树/二叉树的最近公共祖先.java
@@ -0,0 +1,41 @@
+package 树;
+
+import 树.节点.TreeNode;
+
+public class 二叉树的最近公共祖先 {
+    /**
+     * 二叉树的最近公共祖先
+     * 思路：
+     * 三种情况：
+     * 1、p q 一个在左子树 一个在右子树 那么当前节点即是最近公共祖先
+     * 2、p q 都在左子树
+     * 3、p q 都在右子树
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            // p q 一个在左，一个在右
+            return root;
+        }
+        if (left != null) {
+            // p q 都在左子树
+            return left;
+        }
+        if (right != null) {
+            // p q 都在右子树
+            return right;
+        }
+        return null;
+    }
+}

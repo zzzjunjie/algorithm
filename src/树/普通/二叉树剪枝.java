345111f864425ac072853aa04fe1aaf126cdf4ad
zhoujunjie <1367970137@qq.com>
2021-01-08 10:01:05 +0800

    二叉树剪枝

--- src/树/普通/二叉树剪枝.java
+++ src/树/普通/二叉树剪枝.java
@@ -6,6 +6,7 @@ import 树.节点.TreeNode;
  * https://leetcode-cn.com/problems/binary-tree-pruning/
  */
 public class 二叉树剪枝 {
+    //------我真没想到这样写是可以的= =年轻的我也不知道
     public TreeNode pruneTree(TreeNode root) {
        if (root == null){
            return root;

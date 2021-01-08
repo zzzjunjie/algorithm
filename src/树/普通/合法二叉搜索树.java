ad13964e2eb412885e4b01a01eae8eb77df8852f
zhoujunjie <1367970137@qq.com>
2021-01-08 23:06:01 +0800

    合法二叉搜索树

--- src/树/普通/合法二叉搜索树.java
+++ src/树/普通/合法二叉搜索树.java
@@ -26,4 +26,13 @@ public class 合法二叉搜索树 {
         return true;
     }
 
+
+    /**
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long min, long max) {
+        return root == null || (root.val > min && root.val < max && isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max));
+    }*/
 }

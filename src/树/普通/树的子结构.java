69ba0dca1f3e432b9fd3abe2c62bc493dc356d3d
zhoujunjie <1367970137@qq.com>
2021-01-08 09:42:05 +0800

    树的子结构

---
+++ src/树/普通/树的子结构.java
@@ -0,0 +1,26 @@
+package 树.普通;
+
+import sun.reflect.generics.tree.Tree;
+import 树.节点.TreeNode;
+
+/**
+ * https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/comments/
+ */
+public class 树的子结构 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A==null){
+            return false;
+        }
+        return dfs(A,B) || isSubStructure(A.left,B) || isSubStructure(A.right,B);
+    }
+
+    public boolean dfs(TreeNode p, TreeNode q){
+        if (q==null){
+            return true;
+        }
+        if (p == null){
+            return false;
+        }
+        return p.val== q.val && dfs(p.left,q.left) && dfs(p.right,q.right);
+    }
+}

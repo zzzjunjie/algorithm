5d2801bfa277b2d3becc3602d9d7465832e518be
zhoujunjie <1367970137@qq.com>
2021-01-09 09:51:49 +0800

    合法二叉搜索树

---
+++ src/树/普通/合法二叉搜索树.java
@@ -0,0 +1,29 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 合法二叉搜索树 {
+    public boolean isValidBST(TreeNode root) {
+      return dfs(root,null,null);
+    }
+
+    public boolean dfs(TreeNode root,Integer min,Integer max){
+        if (root==null){
+            return true;
+        }
+        if (min!=null && root.val <= min){
+            return false;
+        }
+        if (max != null && root.val >= max){
+            return false;
+        }
+        if (!dfs(root.left,min,root.val)){
+            return false;
+        }
+        if (!dfs(root.right,root.val,max)){
+            return false;
+        }
+        return true;
+    }
+
+}

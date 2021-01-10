1f20d427a975b11b7ab3ba53a9843c4bcce2ef95
zhoujunjie <1367970137@qq.com>
2021-01-10 15:44:46 +0800

    从前序与中序遍历序列构造二叉树

---
+++ src/树/普通/从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,32 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 从前序与中序遍历序列构造二叉树 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length!=inorder.length){
+            throw new RuntimeException();
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i],i);
+        }
+
+        return buildTree(preorder,0,preorder.length-1,map,0,inorder.length-1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int preLeft, int preRight, Map<Integer, Integer> map, int inLeft, int inRight) {
+        if (preLeft>preRight || inLeft>inRight){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[preLeft]);
+        Integer pIdex = map.get(preorder[preLeft]);
+        root.left = buildTree(preorder,preLeft+1, pIdex - inLeft + preLeft,map,inLeft,pIdex-1);
+        root.right = buildTree(preorder,pIdex - inLeft + preLeft+1, preRight,map,pIdex+1,inRight);
+        return root;
+    }
+}

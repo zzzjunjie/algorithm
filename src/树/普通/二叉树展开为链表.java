b4f5dd6b5b1436c3d9ac9f43b8daf8b34e8e7d64
zhoujunjie <1367970137@qq.com>
2021-01-10 16:44:53 +0800

    二叉树展开为链表

---
+++ src/树/普通/二叉树展开为链表.java
@@ -0,0 +1,24 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 二叉树展开为链表 {
+    public void flatten(TreeNode root) {
+      if (root==null){
+          return;
+      }
+      flatten(root.left);
+      flatten(root.right);
+
+      TreeNode node = root.right;
+      root.right = root.left;
+      root.left = null;
+      TreeNode p = root;
+      while (p.right!=null){
+          p = p.right;
+      }
+      p.right = node;
+    }
+
+
+}
---
+++ src/树/普通/从后序与中序遍历序列构造二叉树.java
@@ -0,0 +1,4 @@
+package 树.普通;
+
+public class 从后序与中序遍历序列构造二叉树 {
+}

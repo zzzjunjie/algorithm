cddfe827e4a43be0b11fb0aa37abb8ab0e5c1685
zhoujunjie <1367970137@qq.com>
2021-01-10 13:35:00 +0800

    删点成林

--- src/树/普通/删点成林.java
+++ src/树/普通/删点成林.java
@@ -1,7 +1,10 @@
 package 树.普通;
 
+import sun.reflect.generics.tree.Tree;
 import 树.节点.TreeNode;
 
+import javax.crypto.interfaces.PBEKey;
+import javax.swing.text.rtf.RTFEditorKit;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -9,30 +12,38 @@ import java.util.Set;
 
 public class 删点成林 {
     Set<Integer> set = new HashSet<>();
-    List<TreeNode> ans = new ArrayList<>();
+    List<TreeNode> res = new ArrayList<>();
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        if (root == null) {
+            return res;
+        }
+        for (int i : to_delete) {
+            set.add(i);
+        }
+        if (!set.contains(root.val)) {
+            res.add(root);
+        }
+        dfs(root);
+        return res;
+    }
 
     public TreeNode dfs(TreeNode root) {
-        if (root == null) return null;
-        root.left = dfs(root.left);
+        if (root == null) {
+            return null;
+        }
+
+        root.left = dfs((root.left));
         root.right = dfs(root.right);
         if (set.contains(root.val)) {
-            if (root.left != null)
-                ans.add(root.left);
-            if (root.right != null)
-                ans.add(root.right);
+            if (root.left != null) {
+                res.add(root.left);
+            }
+            if (root.right != null) {
+                res.add(root.right);
+            }
             root = null;
         }
         return root;
-    }
-
-    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-        for (int d : to_delete) {
-            set.add(d);
-        }
-        if (!set.contains(root.val)) {
-            ans.add(root);
-        }
-        dfs(root);
-        return ans;
     }
 }

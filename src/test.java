e6eb53ae94ad7eaaa4ed24c99da45b45d4bc00db
zhoujunjie <1367970137@qq.com>
2021-01-11 13:23:26 +0800

    清除测试代码

--- src/test.java
+++ src/test.java
@@ -1,16 +1,7 @@
 import 树.节点.TreeNode;
 
 public class test {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null){
-            return null;
-        }
-        if (p.val>root.val && q.val>root.val){
-            return lowestCommonAncestor(root.right,p,q);
-        }
-        if (p.val<root.val && q.val<root.val){
-            return lowestCommonAncestor(root.left,p,q);
-        }
-        return root;
+    public void flatten(TreeNode root) {
+
     }
 }

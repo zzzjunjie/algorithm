bf596756e7e949691fd09f57334e5ba26774b703
zhoujunjie <1367970137@qq.com>
2021-01-12 01:05:59 +0800

    二叉搜索树中的插入操作

---
+++ src/树/普通/二叉搜索树中的插入操作.java
@@ -0,0 +1,20 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 二叉搜索树中的插入操作 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root==null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left,val);
+            return root;
+        }else{
+            root.right = insertIntoBST(root.right,val);
+            return root;
+        }
+
+    }
+}

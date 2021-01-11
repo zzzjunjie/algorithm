98b3f261cefce9c599b1ab6c4ce9dd1ee5cc1631
zhoujunjie <1367970137@qq.com>
2021-01-11 13:38:27 +0800

    二叉树的右视图

---
+++ src/树/普通/二叉树的右视图.java
@@ -0,0 +1,39 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-right-side-view/
+ */
+public class 二叉树的右视图 {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root==null){
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (i==size-1){
+                    res.add(poll.val);
+                }
+
+                if (poll.left!=null){
+                    queue.offer(poll.left);
+                }
+                if (poll.right!=null){
+                    queue.offer(poll.right);
+                }
+            }
+        }
+        return res;
+    }
+}

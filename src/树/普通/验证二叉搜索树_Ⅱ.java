df1e68a02dfb6636ddf90015ca44e740921d6a1f
zhoujunjie <1367970137@qq.com>
2021-01-12 21:41:47 +0800

    验证二叉搜索树_Ⅱ

---
+++ src/树/普通/验证二叉搜索树_Ⅱ.java
@@ -0,0 +1,24 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 验证二叉搜索树_Ⅱ {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root,null,null);
+    }
+
+
+    public boolean dfs(TreeNode root,Integer min, Integer max){
+        if (root==null){
+            return true;
+        }
+        if (min!=null && root.val <= min){
+            return false;
+        }
+
+        if (max!=null && root.val >= max){
+            return  false;
+        }
+        return dfs(root.left, min, root.val) && dfs(root.right,root.val,max);
+    }
+}

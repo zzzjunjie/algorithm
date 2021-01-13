8582fa16c97c4b526f8b70cdbb5cd6075b3881b5
zhoujunjie <1367970137@qq.com>
2021-01-13 20:28:46 +0800

    完全二叉树插入器

--- src/树/普通/完全二叉树插入器.java
+++ src/树/普通/完全二叉树插入器.java
@@ -1,11 +1,10 @@
 package 树.普通;
 
-import 树.节点.TreeNode;
-
-import java.util.LinkedList;
-import java.util.Queue;
-
+/**
+ * https://leetcode-cn.com/problems/complete-binary-tree-inserter/submissions/
+ */
 public class 完全二叉树插入器 {
+    /* 自己的解法
     private TreeNode root;
     private TreeNode inNode;
     
@@ -51,4 +50,44 @@ public class 完全二叉树插入器 {
     public TreeNode get_root() {
         return this.root;
     }
+    */
+
+/* 双端队列
+    TreeNode root;
+    Deque<TreeNode> deque;
+    public CBTInserter(TreeNode root) {
+        this.root = root;
+        deque = new LinkedList();
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(root);
+
+        // BFS to populate deque
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left == null || node.right == null)
+                deque.offerLast(node);
+            if (node.left != null)
+                queue.offer(node.left);
+            if (node.right != null)
+                queue.offer(node.right);
+        }
+    }
+
+    public int insert(int v) {
+        TreeNode node = deque.peekFirst();
+        deque.offerLast(new TreeNode(v));
+        if (node.left == null)
+            node.left = deque.peekLast();
+        else {
+            node.right = deque.peekLast();
+            deque.pollFirst();
+        }
+
+        return node.val;
+    }
+
+    public TreeNode get_root() {
+        return root;
+    }
+    */
 }

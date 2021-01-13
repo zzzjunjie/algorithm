e9fb6ad5e7bd7be341928f32fc92c1b25ec17848
zhoujunjie <1367970137@qq.com>
2021-01-13 21:51:01 +0800

    从上到下打印二叉树_III

---
+++ src/树/普通/从上到下打印二叉树_III.java
@@ -0,0 +1,46 @@
+package 树.普通;
+
+import sun.plugin.javascript.navig.Link;
+import 树.节点.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 从上到下打印二叉树_III {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean flage = true;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> ans = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                if (flage){
+                    ans.add(poll.val);
+                }else{
+                    ans.add(0,poll.val);
+
+                }
+
+                if (poll.left!=null){
+                    queue.add(poll.left);
+                }
+
+                if (poll.right!=null){
+                    queue.add(poll.right);
+                }
+            }
+            flage = !flage;
+            res.add(ans);
+        }
+        return res;
+    }
+}

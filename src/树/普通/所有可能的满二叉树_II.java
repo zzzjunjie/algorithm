7503d71cc45e3361596077a2c513ed1fe7a8b84b
zhoujunjie <1367970137@qq.com>
2021-01-13 23:45:41 +0800

    添加注释

--- src/树/普通/所有可能的满二叉树_II.java
+++ src/树/普通/所有可能的满二叉树_II.java
@@ -21,10 +21,15 @@ public class 所有可能的满二叉树_II {
         }
         // 只有单数才可能会构建成满二叉树
         if (N % 2 == 1){
+            // i代表左边有多少个数去组成一棵树
             for (int i = 0; i < N; i++) {
+                // Y代表右边有多少个数去组成一棵树
                 int y = N - 1 - i;
+                // 左子树集合
                 List<TreeNode> left = allPossibleFBT(i);
+                // 右子树集合
                 List<TreeNode> right = allPossibleFBT(y);
+                // 每一个左子树都和右子树对应一次，组成的集合就是N可以生成多少课满二叉树
                 for (TreeNode lTree : left) {
                     for (TreeNode rTree : right) {
                         TreeNode tmp = new TreeNode(0);

529f014abff17f2ff1cbad27d46849d662a08bec
zhoujunjie <1367970137@qq.com>
2021-01-14 09:35:50 +0800

    二叉搜索树中第K小的元素

--- src/树/普通/二叉搜索树中第K小的元素.java
+++ src/树/普通/二叉搜索树中第K小的元素.java
@@ -2,11 +2,8 @@ package 树.普通;
 
 import 树.节点.TreeNode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class 二叉搜索树中第K小的元素 {
+    /* 暴力破解 - -效率极低
     List<Integer> res = new ArrayList<>();
     public int kthSmallest(TreeNode root, int k) {
         dfs(root);
@@ -22,4 +19,25 @@ public class 二叉搜索树中第K小的元素 {
         dfs(root.left);
         dfs(root.right);
     }
+    */
+
+    public int kthSmallest(TreeNode root, int k) {
+        int lCount = findChild(root.left);
+        if (lCount+1 == k){
+            return root.val;
+        }
+
+        if (lCount >= k){
+            return kthSmallest(root.left,k);
+        }
+        return kthSmallest(root.right,k-lCount - 1);
+    }
+
+    public int findChild(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return findChild(root.left) + findChild(root.right) + 1;
+    }
+
 }

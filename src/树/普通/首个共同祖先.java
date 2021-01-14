d751ac861f02c208825b33faaedb417ba2860de9
zhoujunjie <1367970137@qq.com>
2021-01-14 09:56:31 +0800

    首个共同祖先

---
+++ src/树/普通/首个共同祖先.java
@@ -0,0 +1,28 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 首个共同祖先 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root==null){
+            return null;
+        }
+
+        if (root == p || root == q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left!=null && right != null){
+            return root;
+        }
+
+        if (left!=null){
+            return left;
+        }
+
+        return right;
+    }
+}

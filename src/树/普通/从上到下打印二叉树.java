f3313f4156e7693c0c5e6bdb02174489f65116e8
zhoujunjie <1367970137@qq.com>
2021-01-17 16:25:48 +0800

    从上到下打印二叉树

---
+++ src/树/普通/从上到下打印二叉树.java
@@ -0,0 +1,38 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 从上到下打印二叉树 {
+    public int[] levelOrder(TreeNode root) {
+        List<Integer> res  = new ArrayList<>();
+        if (root==null){
+            return new int[0];
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                res.add(poll.val);
+
+                if (poll.left!=null) queue.offer(poll.left);
+                if (poll.right!=null) queue.offer(poll.right);
+            }
+        }
+
+        int[] intRes = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            intRes[i] = res.get(i);
+        }
+        return intRes;
+    }
+}

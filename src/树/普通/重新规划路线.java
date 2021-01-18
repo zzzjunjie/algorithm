87751fb31c65181854124648f458f5492e933d5d
zhoujunjie <1367970137@qq.com>
2021-01-18 09:32:10 +0800

    重新规划路线-添加注释

--- src/树/普通/重新规划路线.java
+++ src/树/普通/重新规划路线.java
@@ -11,8 +11,10 @@ import java.util.Queue;
  */
 public class 重新规划路线 {
     public int minReorder(int n, int[][] connections) {
-        Queue<Integer> vertex=new LinkedList<Integer>();//BFS辅助队列
-        Map<Integer,HashSet<int[]>> graph=new HashMap<>();//存储有向图的顶点以及关联的弧
+        //BFS辅助队列
+        Queue<Integer> vertex=new LinkedList<Integer>();
+        //存储有向图的顶点以及关联的弧
+        Map<Integer,HashSet<int[]>> graph=new HashMap<>();
         int ans=0;
         for(int[] e:connections) {
             if(!graph.containsKey(e[0]))graph.put(e[0],new HashSet<int[]>());
@@ -20,14 +22,19 @@ public class 重新规划路线 {
             if(!graph.containsKey(e[1]))graph.put(e[1],new HashSet<int[]>());
             graph.get(e[1]).add(e);
         }
-        vertex.offer(0);//从顶点0开始遍历该有向图
+        //从顶点0开始遍历该有向图
+        vertex.offer(0);
         while(!vertex.isEmpty()) {
-            int ver=vertex.poll();//出队访问
-            HashSet<int[]> hashtemp=graph.get(ver);//暂存该顶点关联的所有边
-            graph.remove(ver);//从graph中移除已经访问过的顶点
+            //出队访问
+            int ver=vertex.poll();
+            //暂存该顶点关联的所有边
+            HashSet<int[]> hashtemp=graph.get(ver);
+            //从graph中移除已经访问过的顶点
+            graph.remove(ver);
             for(int[] e:hashtemp) {
                 if(e[0]!=ver&&graph.containsKey(e[0]))vertex.offer(e[0]);
-                if(e[1]!=ver&&graph.containsKey(e[1])) {//e[1]!=ver,说明e[0]=ver,则该边是逆向边
+                //e[1]!=ver,说明e[0]=ver,则该边是逆向边
+                if(e[1]!=ver&&graph.containsKey(e[1])) {
                     vertex.offer(e[1]);
                     ans++;
                 }

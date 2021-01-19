4125e82b251fe96d91aac48e4bb0246687888c8f
zhoujunjie <1367970137@qq.com>
2021-01-19 09:45:24 +0800

    打家劫舍_III

---
+++ src/树/普通/打家劫舍_III.java
@@ -0,0 +1,29 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 解法：https://leetcode-cn.com/problems/house-robber-iii/solution/san-chong-fang-fa-jie-jue-shu-xing-dong-tai-gui-hu/
+ */
+public class 打家劫舍_III {
+    Map<TreeNode, Integer> f = new HashMap<>();
+    Map<TreeNode, Integer> g = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
+        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
+    }
+}

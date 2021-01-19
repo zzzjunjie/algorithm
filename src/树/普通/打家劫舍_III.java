eb3a152ed8ed7d599dabe4b8192d802ddabb9146
zhoujunjie <1367970137@qq.com>
2021-01-19 09:55:23 +0800

    打家劫舍_III

--- src/树/普通/打家劫舍_III.java
+++ src/树/普通/打家劫舍_III.java
@@ -9,21 +9,40 @@ import java.util.Map;
  * 解法：https://leetcode-cn.com/problems/house-robber-iii/solution/san-chong-fang-fa-jie-jue-shu-xing-dong-tai-gui-hu/
  */
 public class 打家劫舍_III {
-    Map<TreeNode, Integer> f = new HashMap<>();
-    Map<TreeNode, Integer> g = new HashMap<>();
-
     public int rob(TreeNode root) {
-        dfs(root);
-        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+        int[] result = robInternal(root);
+        return Math.max(result[0], result[1]);
     }
 
-    public void dfs(TreeNode node) {
-        if (node == null) {
-            return;
+    public int[] robInternal(TreeNode root) {
+        if (root == null) {
+            return new int[2];
         }
-        dfs(node.left);
-        dfs(node.right);
-        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
-        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
+        int[] result = new int[2];
+
+        int[] left = robInternal(root.left);
+        int[] right = robInternal(root.right);
+
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = left[0] + right[0] + root.val;
+
+        return result;
     }
+//    Map<TreeNode, Integer> f = new HashMap<>();
+//    Map<TreeNode, Integer> g = new HashMap<>();
+//
+//    public int rob(TreeNode root) {
+//        dfs(root);
+//        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+//    }
+//
+//    public void dfs(TreeNode node) {
+//        if (node == null) {
+//            return;
+//        }
+//        dfs(node.left);
+//        dfs(node.right);
+//        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
+//        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
+//    }
 }

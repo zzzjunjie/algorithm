8ae6bff9c03451456f63ca04973ea06cb58b18df
zhoujunjie <1367970137@qq.com>
2021-01-20 09:28:00 +0800

    把二叉搜索树转换为累加树

---
+++ src/树/普通/把二叉搜索树转换为累加树.java
@@ -0,0 +1,50 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 把二叉搜索树转换为累加树 {
+    /** 自己的第一个思路
+    List<Integer> list = new ArrayList<>();
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return help(root);
+    }
+
+    public TreeNode help(TreeNode root){
+        if (root==null){
+            return null;
+        }
+        int val = root.val;
+        for (Integer item : list) {
+            if (item>val) {
+                root.val += item;
+            }
+        }
+        root.left = help(root.left);
+        root.right = help(root.right);
+        return root;
+    }
+
+    public void dfs(TreeNode root){
+        if (root==null) {
+            return ;
+        }
+
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }*/
+
+    // 官方参考思路
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}

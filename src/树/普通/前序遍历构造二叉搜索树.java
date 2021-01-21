b5336c83b6af30c7be871b69e1e88f34f1cd78cb
zhoujunjie <1367970137@qq.com>
2021-01-21 19:37:35 +0800

    前序遍历构造二叉搜索树

---
+++ src/树/普通/前序遍历构造二叉搜索树.java
@@ -0,0 +1,43 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 前序遍历构造二叉搜索树 {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if (preorder.length<=0){
+            return null;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i : preorder) {
+            queue.offer(i);
+        }
+        TreeNode res = new TreeNode(queue.poll());
+        while (!queue.isEmpty()){
+            Integer poll = queue.poll();
+            insert(res,poll);
+        }
+        return res;
+    }
+
+    public void insert(TreeNode root,int v){
+        if (root==null){
+            return;
+        }
+        if (root.val > v && root.left ==null){
+            root.left = new TreeNode(v);
+            return ;
+        }
+        if (root.val < v && root.right ==null){
+            root.right = new TreeNode(v);
+            return ;
+        }
+        if (root.val > v){
+            insert(root.left,v);
+        }else{
+            insert(root.right,v);
+        }
+    }
+}

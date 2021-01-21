286f3710db844d5aab6d1b6dd5ebbb601470efcb
zhoujunjie <1367970137@qq.com>
2021-01-21 19:40:09 +0800

    前序遍历构造二叉搜索树

--- src/树/普通/前序遍历构造二叉搜索树.java
+++ src/树/普通/前序遍历构造二叉搜索树.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class 前序遍历构造二叉搜索树 {
+    /*自己的解法
     public TreeNode bstFromPreorder(int[] preorder) {
         if (preorder.length<=0){
             return null;
@@ -39,5 +40,24 @@ public class 前序遍历构造二叉搜索树 {
         }else{
             insert(root.right,v);
         }
+    }*/
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root = null;
+        for (int i = 0; i < preorder.length; i++) {
+            root=add(root, preorder[i]);
+        }
+        return root;
+    }
+
+    public TreeNode add(TreeNode root, int num) {
+        if (root == null) {
+            return new TreeNode(num);
+        }
+        if (root.val > num) {
+            root.left=add(root.left, num);
+        } else {
+            root.right = add(root.right, num);
+        }
+        return root;
     }
 }

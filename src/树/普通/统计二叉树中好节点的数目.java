e8b50ca628c7ffdc0a39995eeefb2b32f06ecc65
zhoujunjie <1367970137@qq.com>
2021-01-21 20:00:47 +0800

    统计二叉树中好节点的数目

---
+++ src/树/普通/统计二叉树中好节点的数目.java
@@ -0,0 +1,28 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 统计二叉树中好节点的数目 {
+
+    int res = 0;
+    int max = Integer.MIN_VALUE;
+    public int goodNodes(TreeNode root) {
+        max = root.val;
+        dfs(root);
+        return res;
+    }
+
+    public void dfs(TreeNode root){
+        if (root==null){
+            return;
+        }
+        if (root.val >= max){
+            res ++;
+        }
+        int tmp = max;
+        max = Math.max(max,root.val);
+        dfs(root.left);
+        dfs(root.right);
+        max = tmp;
+    }
+}

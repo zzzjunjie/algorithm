463f016f1a086af6827112a5600ccb61ce1e9972
zhoujunjie <1367970137@qq.com>
2021-01-22 00:44:47 +0800

    二叉树寻路

--- src/树/普通/二叉树寻路.java
+++ src/树/普通/二叉树寻路.java
@@ -1,32 +1,30 @@
 package 树.普通;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class 二叉树寻路 {
     public List<Integer> pathInZigZagTree(int label) {
-        //0.初始化存放结果的变量
-        ArrayList<Integer> integers = new ArrayList<>();
-        //2.计算label所在的层
-        int a = (int) (Math.log(label) / Math.log(2));
-        //5.循环直到遇到特殊情况1
-        while (label > 1) {
-            //3.将label的结果添加到数组中
-            integers.add(label);
-            //4.计算下一个label的值
-            label = (int) (3 * Math.pow(2, --a) - label / 2 - 1);
+        List<Integer> list = new ArrayList<>();
+        int h = (int) Math.log(label-2)+1;
+//        int h = (int)(Math.log(label)/Math.log(2))+1;
+        while(label>1 && h>0){
+            list.add(label);
+            label =(int)(Math.pow(2,h)-1-label+Math.pow(2,h-1))/2;
+            h--;
         }
-        //6.添加特殊情况 1
-        integers.add(1);
-        //7.翻转数组
-        Collections.reverse(integers);
-        //1.返回结果
-        return integers;
+        list.add(1);
+        list.sort(new Comparator<Integer>(){
+            public int compare(Integer o1,Integer o2){
+                return o1-o2;
+            }
+        });
+
+        return list;
     }
 
     public static void main(String[] args) {
-        int a = (int) (Math.log(7) / Math.log(2));
-        System.out.println(a);
+        System.out.println(Math.log(Math.E));
     }
 }

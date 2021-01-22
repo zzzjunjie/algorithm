c48267970cb54be53da08170239de9311e25716b
zhoujunjie <1367970137@qq.com>
2021-01-22 23:10:00 +0800

    在每个树行中找最大值

---
+++ src/树/普通/在每个树行中找最大值.java
@@ -0,0 +1,33 @@
+package 树.普通;
+
+import javafx.print.PageOrientation;
+import 树.节点.TreeNode;
+
+import java.util.*;
+
+public class 在每个树行中找最大值 {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root==null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i <size; i++) {
+                TreeNode poll = queue.poll();
+                max = Math.max(max,poll.val);
+                if (poll.left!=null){
+                    queue.offer(poll.left);
+                }
+                if (poll.right!=null){
+                    queue.offer(poll.right);
+                }
+            }
+            res.add(max);
+        }
+        return res;
+    }
+}

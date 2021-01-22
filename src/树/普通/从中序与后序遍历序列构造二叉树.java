7aa135238fb972877fefb42360abef02f54bff62
zhoujunjie <1367970137@qq.com>
2021-01-22 23:55:20 +0800

    从后序与中序遍历序列构造二叉树

---
+++ src/树/普通/从中序与后序遍历序列构造二叉树.java
@@ -0,0 +1,30 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 从中序与后序遍历序列构造二叉树 {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return helper(inorder, postorder, postorder.length - 1, 0, inorder.length - 1);
+    }
+
+    public TreeNode helper(int[] inorder, int[] postorder, int postEnd, int inStart, int inEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+
+        int currentVal = postorder[postEnd];
+        TreeNode current = new TreeNode(currentVal);
+
+        int inIndex = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == currentVal) {
+                inIndex = i;
+            }
+        }
+        TreeNode left = helper(inorder, postorder, postEnd - (inEnd- inIndex) - 1, inStart, inIndex - 1);
+        TreeNode right = helper(inorder, postorder, postEnd - 1, inIndex + 1, inEnd);
+        current.left = left;
+        current.right = right;
+        return current;
+    }
+}

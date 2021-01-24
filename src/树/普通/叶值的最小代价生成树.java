73000b7d0c7c1a77ce4aaf944ba55e275ad9bd73
zhoujunjie <1367970137@qq.com>
2021-01-24 18:13:59 +0800

    叶值的最小代价生成树

---
+++ src/树/普通/叶值的最小代价生成树.java
@@ -0,0 +1,24 @@
+package 树.普通;
+
+import java.util.Stack;
+
+public class 叶值的最小代价生成树 {
+    public int mctFromLeafValues(int[] arr) {
+        //1. 如果栈顶元素比当前元素小，弹出栈顶元素，
+        //   栈顶元素与当前元素和栈顶下一个元素中得最小值组合
+        //2. 如果栈顶元素比当前元素大，入栈
+        Stack<Integer> stack = new Stack<>();
+        stack.push(Integer.MAX_VALUE);//哨兵
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++){
+            while (stack.peek() < arr[i])
+                ans += stack.pop()*Math.min(arr[i],stack.peek());
+            stack.push(arr[i]);
+        }
+
+        while(stack.size() > 2)
+            ans += stack.pop()*stack.peek();
+
+        return ans;
+    }
+}

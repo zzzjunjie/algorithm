cb8d6e1b7a689d6f33454b8442cf4d1946fe883f
zhoujunjie <1367970137@qq.com>
2021-01-25 21:52:17 +0800

    重建二叉树

---
+++ src/树/普通/重建二叉树.java
@@ -0,0 +1,29 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 重建二叉树 {
+    Map<Integer,Integer> map = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i],i);
+        }
+
+        return helper(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+    }
+
+    private TreeNode helper(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd || inStart > inEnd){
+            return null;
+        }
+
+        int index = map.get(preorder[preStart]);
+        TreeNode root = new TreeNode(preorder[preStart]);
+        root.left = helper(preorder,preStart+1,index-inStart+preStart,inorder,inStart,index-1);
+        root.right = helper(preorder,index-inStart+preStart+1,preEnd,inorder,index+1,inEnd);
+        return root;
+    }
+}

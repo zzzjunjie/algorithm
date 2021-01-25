2b6841f8742c0e33df685ce90903e771f445991d
zhoujunjie <1367970137@qq.com>
2021-01-26 00:04:07 +0800

    二叉树的最近公共祖先

---
+++ src/树/普通/二叉树的最近公共祖先.java
@@ -0,0 +1,22 @@
+package 树.普通;
+
+import 树.节点.TreeNode;
+
+public class 二叉树的最近公共祖先 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left!=null && right!=null){
+            return root;
+        }
+
+        if (left!=null){
+            return left;
+        }
+        return right;
+    }
+}

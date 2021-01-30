cdfa882a62b7630054d848d9d9695f02a60b0b1d
zhoujunjie <1367970137@qq.com>
2021-01-30 23:04:44 +0800

    省份数量

--- src/并查集/省份数量.java
+++ src/并查集/省份数量.java
@@ -5,36 +5,37 @@ package 并查集;
  */
 public class 省份数量 {
     public int findCircleNum(int[][] isConnected) {
-        int provinces = isConnected.length;
-        int[] parent = new int[provinces];
-        for (int i = 0; i < provinces; i++) {
-            parent[i] = i;
+        int len = isConnected.length;
+        int[] parent = new int[len];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i]=i;
         }
-        for (int i = 0; i < provinces; i++) {
-            for (int j = i + 1; j < provinces; j++) {
-                if (isConnected[i][j] == 1) {
-                    union(parent, i, j);
+        for (int i = 0; i < isConnected.length; i++) {
+            int[] ints = isConnected[i];
+            for (int j = i+1; j < ints.length; j++) {
+                if (ints[j]==1){
+                    union(parent,ints[i],ints[j]);
                 }
             }
         }
-        int circles = 0;
-        for (int i = 0; i < provinces; i++) {
-            if (parent[i] == i) {
-                circles++;
+        int count = 0;
+        for (int i = 0; i < parent.length; i++) {
+            if (parent[i]==i){
+                count++;
             }
         }
-        return circles;
+        return count;
     }
 
-    public void union(int[] parent, int index1, int index2) {
-        parent[find(parent, index1)] = find(parent, index2);
+    public void union(int[] parent,int i,int j){
+        parent[find(parent,i)] = find(parent,j);
     }
 
-    public int find(int[] parent, int index) {
-        if (parent[index] != index) {
-            parent[index] = find(parent, parent[index]);
+    public int find(int[] parent,int targe){
+        if (parent[targe] != targe) {
+            parent[targe]= find(parent,parent[targe]);
         }
-        return parent[index];
+        return parent[targe];
     }
 
 }

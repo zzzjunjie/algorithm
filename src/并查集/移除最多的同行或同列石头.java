0df7e78e5a5b307aaf35352473825b3c2deaf35c
zhoujunjie <1367970137@qq.com>
2021-02-01 21:28:03 +0800

    移除最多的同行或同列石头

--- src/并查集/移除最多的同行或同列石头.java
+++ src/并查集/移除最多的同行或同列石头.java
@@ -1,9 +1,58 @@
 package 并查集;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class 移除最多的同行或同列石头 {
+    public int removeStones(int[][] stones) {
+        int[] father = new int[stones.length];
+        //初始化 自己是自己的上级
+        for(int i=0; i<stones.length; i++){
+            father[i] = i;
+        }
+
+        //查询 合并
+        for(int i=0; i<stones.length; i++){
+            for(int j=i+1; j<stones.length; j++){
+                int x1 = stones[i][0];
+                int y1 = stones[i][1];
+                int x2 = stones[j][0];
+                int y2 = stones[j][1];
+                if(x1 == x2 || y1 == y2){
+                    union(father, i, j);
+                }
+            }
+        }
+
+        //计算连通分量个数 hash数据结构查询快 set没有重复元素
+        HashSet<Integer> res = new HashSet<>();
+        for(int i=0; i<father.length; i++){
+            res.add(findFather(father, i));
+        }
+        //结果= 所有石头数 - 连通分量个数
+        return stones.length - res.size();
+    }
+
+    //合并连通分量
+    public void union(int[] father, int x, int y){
+        int xFather = findFather(father, x);
+        int yFather = findFather(father ,y);
+        if(xFather != yFather){
+            father[xFather] = yFather;
+        }
+    }
+
+    //找上级
+    public int findFather(int[] father, int x){
+        while(father[x] != x){
+            father[x] = father[father[x]];
+            x = father[x];
+        }
+        return x;
+    }
+
+    /** 官方解法
     public int removeStones(int[][] stones) {
         UnionFind unionFind = new UnionFind();
 
@@ -55,4 +104,5 @@ public class 移除最多的同行或同列石头 {
             count--;
         }
     }
+     */
 }

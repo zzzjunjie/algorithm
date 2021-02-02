03412c7aedf4281c361f34b6ed8f120cf2ff299d
zhoujunjie <1367970137@qq.com>
2021-02-02 21:29:56 +0800

    连续子数组的最大和

--- src/动态规划/简单/连续子数组的最大和.java
+++ src/动态规划/简单/连续子数组的最大和.java
@@ -3,6 +3,12 @@ package 动态规划.简单;
 public class 连续子数组的最大和 {
 
     public int maxSubArray(int[] nums) {
-
+        int pre = 0;
+        int res = nums[0];
+        for (int num : nums) {
+            pre = Math.max(num + pre,num);
+            res = Math.max(pre,res);
+        }
+        return res;
     }
 }

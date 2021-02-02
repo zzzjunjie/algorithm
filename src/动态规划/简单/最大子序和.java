8107dace670fa628507c557bfd3006c94d4d8994
zhoujunjie <1367970137@qq.com>
2021-02-02 19:57:11 +0800

    最大子序和

---
+++ src/动态规划/简单/最大子序和.java
@@ -0,0 +1,14 @@
+package 动态规划.简单;
+
+//https://leetcode-cn.com/problems/maximum-subarray/
+public class 最大子序和 {
+    public int maxSubArray(int[] nums) {
+        int pre = 0;
+        int res = nums[0];
+        for (int num : nums) {
+            pre = Math.max(num + pre,num);
+            res = Math.max(pre,res);
+        }
+        return res;
+    }
+}

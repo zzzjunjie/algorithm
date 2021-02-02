20e58d7ddfe537f5f3a43b8e03547fe8a7c5bd58
zhoujunjie <1367970137@qq.com>
2021-02-02 20:58:41 +0800

    买卖股票的最佳时机

---
+++ src/动态规划/简单/买卖股票的最佳时机.java
@@ -0,0 +1,16 @@
+package 动态规划.简单;
+
+public class 买卖股票的最佳时机 {
+    public int maxProfit(int prices[]) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min){
+                min = prices[i];
+            }else if (prices[i] - min > max){
+                max = prices[i] - min;
+            }
+        }
+        return max;
+    }
+}

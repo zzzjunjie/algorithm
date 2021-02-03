bfe2faee11581f54761e42aa0df165c08441c637
zhoujunjie <1367970137@qq.com>
2021-02-03 19:50:00 +0800

    判断子序列

---
+++ src/动态规划/简单/判断子序列.java
@@ -0,0 +1,12 @@
+package 动态规划.简单;
+
+public class 判断子序列 {
+    public boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for (char c : s.toCharArray()){
+            index = t.indexOf(c, index+1);
+            if (index == -1) return false;
+        }
+        return true;
+    }
+}

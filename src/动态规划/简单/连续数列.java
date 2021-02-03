ab174b0c0749f58ef754e57689d3f4c5d1734376
zhoujunjie <1367970137@qq.com>
2021-02-03 19:13:36 +0800

    连续数列

---
+++ src/动态规划/简单/连续数列.java
@@ -0,0 +1,14 @@
+package 动态规划.简单;
+
+// [-2,1,-3,4,-1,2,1,-5,4] 输出:6
+public class 连续数列 {
+    public int maxSubArray(int[] nums) {
+        int pre = 0;
+        int res = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            pre = Math.max(pre + nums[i],nums[i]);
+            res = Math.max(pre,res);
+        }
+        return res;
+    }
+}

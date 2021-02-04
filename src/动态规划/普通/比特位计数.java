a40cbf5a63f90051577942aa78847009168b1326
zhoujunjie <1367970137@qq.com>
2021-02-04 09:34:45 +0800

    比特位计数

---
+++ src/动态规划/普通/比特位计数.java
@@ -0,0 +1,19 @@
+package 动态规划.普通;
+
+public class 比特位计数 {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        int i = 0, b = 1;
+        // [0, b) is calculated
+        while (b <= num) {
+            // generate [b, 2b) or [b, num) from [0, b)
+            while(i < b && i + b <= num){
+                ans[i + b] = ans[i] + 1;
+                ++i;
+            }
+            i = 0;   // reset i
+            b <<= 1; // b = 2b
+        }
+        return ans;
+    }
+}

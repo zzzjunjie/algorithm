4b0206cb893e969482a547b25e4f316ff289c15e
zhoujunjie <1367970137@qq.com>
2021-03-01 12:02:47 +0800

    青蛙跳台阶

---
+++ src/剑指offer/青蛙跳台阶.java
@@ -0,0 +1,19 @@
+package 剑指offer;
+
+public class 青蛙跳台阶 {
+    public static int func(int n){
+        if (n==1){
+            return 1;
+        }
+        int ret = 0;
+        int a = 1;
+        for (int i = 2; i <n+1 ; i++) {
+            ret = 2 * a;
+            a =ret;
+        }
+        return ret;
+    }
+    public static void main(String[] args) {
+        System.out.println(func(10));
+    }
+}

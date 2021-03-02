348701e1bcb99b39556668d5e36c05951a115ad0
zhoujunjie <1367970137@qq.com>
2021-03-02 16:21:08 +0800

    双栈模拟队列

---
+++ src/剑指offer/CQueue.java
@@ -0,0 +1,35 @@
+package 剑指offer;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class CQueue {
+
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new LinkedList<>();
+        stack2 = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        // 如果第二个栈为空
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.isEmpty()) {
+            return -1;
+        } else {
+            int deleteItem = stack2.pop();
+            return deleteItem;
+        }
+    }
+
+}

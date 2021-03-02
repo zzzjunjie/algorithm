8eccd96f732f49d5a1b870db1fd13db6e069468f
zhoujunjie <1367970137@qq.com>
2021-03-02 14:39:00 +0800

    数组重数

---
+++ src/剑指offer/数组重数.java
@@ -0,0 +1,29 @@
+package 剑指offer;
+
+public class 数组重数 {
+    public static int findRepeatNumber(int[] nums) {
+        if (nums.length<0){
+            return -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i){
+
+                if (nums[i] == nums[nums[i]]){
+                    return nums[i];
+                }
+
+
+                int tmp = nums[i];
+                nums[i] = nums[tmp];
+                nums[tmp] = tmp;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,0,2,5,3};
+        int repeatNumber = findRepeatNumber(arr);
+        System.out.println(repeatNumber);
+    }
+}

9318b3596bc15beeb929ab07b7e2b6e4261642e0
zhoujunjie <1367970137@qq.com>
2021-03-03 16:33:42 +0800

    判断二叉搜索树的后序遍历序列

--- src/排序算法/希尔排序.java
+++ src/排序算法/希尔排序.java
@@ -19,19 +19,40 @@ public class 希尔排序 {
    * @return
    */
   public static int[] ShellSort(int[] array) {
-    int len = array.length;
-    int temp, gap = len / 2;
-    while (gap > 0) {
-      for (int i = gap; i < len; i++) {
-        temp = array[i];
-        int preIndex = i - gap;
-        while (preIndex >= 0 && array[preIndex] > temp) {
-          array[preIndex + gap] = array[preIndex];
-          preIndex -= gap;
+//    int len = array.length;
+//    int temp, gap = len / 2;
+//    while (gap > 0) {
+//      for (int i = gap; i < len; i++) {
+//        temp = array[i];
+//        int preIndex = i - gap;
+//        while (preIndex >= 0 && array[preIndex] > temp) {
+//          array[preIndex + gap] = array[preIndex];
+//          preIndex -= gap;
+//        }
+//        array[preIndex + gap] = temp;
+//      }
+//      gap /= 2;
+//    }
+//    return array;
+
+    int len  = array.length;
+    if (len<=0){
+      return new int[0];
+    }
+
+    int tmp = 0;
+    int gap = len / 2;
+    while (gap > 0 ){
+      for (int i = gap; i <len ; i++) {
+        tmp = array[i];
+        int index = i - gap;
+        while (index >=0 && array[index] > tmp){
+          array[gap + index] = array[index];
+          index = index - gap;
         }
-        array[preIndex + gap] = temp;
+        array[gap + index] = tmp;
       }
-      gap /= 2;
+      gap = gap / 2;
     }
     return array;
   }

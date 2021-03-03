6a1cd47319f48083a715b4cbbab0d099df6cd067
zhoujunjie <1367970137@qq.com>
2021-03-03 18:17:32 +0800

    复杂链表的复制

---
+++ src/剑指offer/复杂链表的复制.java
@@ -0,0 +1,18 @@
+package 剑指offer;
+
+public class 复杂链表的复制 {
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+    public Node copyRandomList(Node head) {
+
+    }
+}

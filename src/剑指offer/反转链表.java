190d039bbde5ac79c90932221f91e17b4b10c358
zhoujunjie <1367970137@qq.com>
2021-03-03 17:28:05 +0800

    反转链表

---
+++ src/剑指offer/反转链表.java
@@ -0,0 +1,53 @@
+package 剑指offer;
+
+ class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+     @Override
+     public String toString() {
+         return "ListNode{" +
+                 "val=" + val +
+                 ", next=" + next +
+                 '}';
+     }
+ }
+public class 反转链表 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        System.out.println(reverseList(l1));
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null){
+            return null;
+        }
+        if (head.next == null){
+            return head;
+        }
+        ListNode l = head;
+        ListNode m = l.next;
+        ListNode r = m.next;
+        l.next = null;
+        while (r!=null){
+            m.next = l;
+            l = m;
+            m = r;
+            r =r.next;
+        }
+        m.next = l;
+        return m;
+    }
+}

79218f09c25c1ca6237e1222e6a6a09264f767cb
zhoujunjie <1367970137@qq.com>
2021-03-05 13:58:39 +0800

    链表中倒数第k个节点

---
+++ src/剑指offer/链表中倒数第k个节点.java
@@ -0,0 +1,23 @@
+package 剑指offer;
+
+public class 链表中倒数第k个节点 {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode low = head;
+        ListNode hight = head;
+        for (int i = 0; i < k; i++) {
+            hight = hight.next;
+        }
+        while (hight!=null){
+            hight= head.next;
+            low = low.next;
+        }
+        return low;
+    }
+
+}

001b67df5901708929712f342cce4c3aeb1acf76
zhoujunjie <1367970137@qq.com>
2021-03-26 00:30:33 +0800

    合并两个有序链表

---
+++ src/腾讯50道精选/合并两个有序链表.java
@@ -0,0 +1,44 @@
+package 腾讯50道精选;
+
+public class 合并两个有序链表 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode cur = root;
+        while (l1 != null && l2 != null) {
+            if (l1.val<l2.val){
+                cur.next = new ListNode(l1.val);
+                l1 = l1.next;
+            }else{
+                cur.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1!=null){
+            cur.next = l1;
+        }
+
+        if (l2!=null){
+            cur.next = l2;
+        }
+
+        return root.next;
+    }
+}

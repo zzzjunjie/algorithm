a119efaa4b8002e6bde335f0dd456934a79aca9f
zhoujunjie <1367970137@qq.com>
2021-03-26 00:30:43 +0800

    回文数

---
+++ src/腾讯50道精选/回文数.java
@@ -0,0 +1,18 @@
+package 腾讯50道精选;
+
+public class 回文数 {
+    public boolean isPalindrome(int x) {
+        if (x < 0 ||x % 10 == 0) {
+            return false;
+        }
+
+        int tmp = 0;
+        while (x > tmp) {
+            tmp = tmp * 10 + x % 10;
+            x = x / 10;
+        }
+        return x == tmp || x == tmp/10;
+    }
+}
+
+

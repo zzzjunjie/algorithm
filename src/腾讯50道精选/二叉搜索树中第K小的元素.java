3f8f6eb51e7a7912c8c0eab27e9496d59e4487ea
zhoujunjie <1367970137@qq.com>
2021-03-30 00:42:42 +0800

    二叉搜索树中第K小的元素

---
+++ src/腾讯50道精选/二叉搜索树中第K小的元素.java
@@ -0,0 +1,56 @@
+package 腾讯50道精选;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class 二叉搜索树中第K小的元素 {
+
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> arr) {
+        if (root == null) return arr;
+        inorder(root.left, arr);
+        arr.add(root.val);
+        inorder(root.right, arr);
+        return arr;
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        ArrayList<Integer> nums = inorder(root, new ArrayList<Integer>());
+        return nums.get(k - 1);
+    }
+
+
+    public int kthSmallest2(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        while (true){
+            while (root!=null){
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if (--k == 0) {
+                return root.val;
+            }
+            root = root.right;
+        }
+    }
+}

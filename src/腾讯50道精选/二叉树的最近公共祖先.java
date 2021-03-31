fcebbea0e61d8662f32b1b73a44b93b359ca8c43
zhoujunjie <1367970137@qq.com>
2021-04-01 00:36:35 +0800

    二叉树的最近公共祖先

---
+++ src/腾讯50道精选/二叉树的最近公共祖先.java
@@ -0,0 +1,50 @@
+package 腾讯50道精选;
+
+public class 二叉树的最近公共祖先 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    // 递归解法
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor1(root.left, p, q);
+        TreeNode right = lowestCommonAncestor1(root.right, p, q);
+
+        if (left!=null && right!=null){
+            return root;
+        }
+
+        if (left == null){
+            return right;
+        }
+        return left;
+    }
+
+
+    // 递归解法2
+    private TreeNode ans;
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+}

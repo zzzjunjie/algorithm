bccd75056ca078ba91ace6a81a4b530f87b60008
zhoujunjie <1367970137@qq.com>
2021-04-07 01:10:13 +0800

    字符串相乘

---
+++ src/腾讯50道精选/字符串相乘.java
@@ -0,0 +1,24 @@
+package 腾讯50道精选;
+
+public class 字符串相乘 {
+    public String multiply(String n1, String n2) {
+        int n = n1.length(), m = n2.length();
+        int[] res = new int[n + m];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int a = n1.charAt(i) - '0';
+                int b = n2.charAt(j) - '0';
+                int r = a * b;
+                r += res[i + j + 1];
+                res[i + j + 1] = r % 10;
+                res[i + j] += r / 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n + m; i++) {
+            if (sb.length() == 0 && res[i] == 0) continue;
+            sb.append(res[i]);
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}

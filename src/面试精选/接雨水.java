248e79bb783f52e44c62fc0d7d91b3ac11b19dd1
zhoujunjie <1367970137@qq.com>
2021-04-09 01:17:47 +0800

    接雨水

---
+++ src/面试精选/接雨水.java
@@ -0,0 +1,34 @@
+package 面试精选;
+
+public class 接雨水 {
+    public static int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={0,1,0,2,1,0,1,3,2,1,2,1};
+        int trap = trap(arr);
+        System.out.println(trap);
+    }
+}

b3aec7a33cc71cb9dcf44d061319e36fe367e20a
zhoujunjie <1367970137@qq.com>
2021-04-23 20:02:03 +0800

    寻路算法

---
+++ src/寻路算法/PathFinder.java
@@ -0,0 +1,66 @@
+package 寻路算法;
+
+
+import 寻路算法.jsp.JPSFinder;
+import 寻路算法.map.MapData;
+import 寻路算法.map.Node;
+
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
+
+public class PathFinder {
+
+	public static void main(String[] args) {
+		// 测试
+//		test();
+		// 地图数据
+		MapData mapData = new MapData();
+		// JPS
+		Finder finder = new JPSFinder(mapData);
+		// 1-map
+//		long curr = System.currentTimeMillis();
+//		for (int i = 0; i < 10000; i++) {			
+//			List<Node> paths = finder.findPath(new Node(1, 39), new Node(99, 65));
+//			List<Node> paths = finder.findPath(new Node(5, 2), new Node(5, 6));
+//		}
+//		System.out.println("耗时：" + (System.currentTimeMillis() - curr));
+		// 1-map
+//		List<Node> paths = finder.findPath(new Node(1, 39), new Node(99, 65));
+		// 2-map
+		List<Node> paths = finder.findPath(new Node(4, 1), new Node(4, 6));
+		// 3-map
+//		List<Node> paths = finder.findPath(new Node(6, 2), new Node(3, 6));
+		System.out.println(paths);
+	}
+	
+	private static void test() {
+		// 优先级队列
+		PriorityQueue<Node> queue = new PriorityQueue<>();
+		Node node1 = new Node(1, 1);
+		node1.f = 2;
+		Node node2 = new Node(2, 2);
+		node2.f = 1;
+		Node node3 = new Node(3, 3);
+		node3.f = 3;
+		queue.add(node1);
+		queue.add(node2);
+		queue.add(node3);
+		node2.f = 4;
+		queue.remove(node2);
+		queue.add(node2);
+		System.out.println(queue.peek());
+		
+		// TreeSet
+		TreeSet<Node> treeset = new TreeSet<>();
+		treeset.add(node1);
+		treeset.add(node2);
+		treeset.add(node3);
+		System.out.println(treeset.first());
+		node2.f = 1;
+		treeset.add(node2);
+		System.out.println(treeset.first());
+	}
+	
+}
---
+++ src/寻路算法/map/Node.java
@@ -0,0 +1,61 @@
+package 寻路算法.map;
+
+public class Node implements Comparable<Node> {
+	
+	public int x;
+	public int y;
+	
+	public int hashCode;
+	
+	public double g;
+	public double f;
+	
+	public Node pre;
+	
+	public Node() {
+		
+	}
+	
+	public Node(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.hashCode = (x << 16) + y;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.hashCode;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// 直接判断
+		Node other = (Node) obj;
+		return x == other.x && y == other.y;
+	}
+	
+	@Override
+	public int compareTo(Node o) {
+		if (this == o)
+			return 0;
+		int v = Integer.compare(this.hashCode, o.hashCode);
+		if (v == 0)
+			return 0;
+		if (Math.abs(this.f - o.f) < 0.01 ) {
+			// 接着比较，保证compareTo和equals一致(即comparaTo返回0的情况，equals一定返回true，TreeSet要求的)
+			return v;
+		}
+		else if (this.f < o.f) {
+			return -1;
+		}
+		else {
+			return 1;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "[x=" + x + ", y=" + y + "]";
+	}
+
+}

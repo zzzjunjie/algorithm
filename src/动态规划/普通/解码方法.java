8d31558e54acfe6426335d861df5eb244d16dd4d
zhoujunjie <1367970137@qq.com>
2021-05-25 20:51:15 +0800

    解码方法

---
+++ src/动态规划/普通/解码方法.java
@@ -0,0 +1,22 @@
+package 动态规划.普通;
+
+
+// https://leetcode-cn.com/problems/decode-ways/
+public class 解码方法 {
+
+	public int numDecodings(String s) {
+		int length = s.length();
+		int[] dp = new int[length + 1];
+		dp[0] = 1;
+		for (int i = 1; i < dp.length; i++) {
+			if (s.charAt(i - 1) != '0') {
+				dp[i] += dp[i-1];
+			}
+			if (i > 1 && s.charAt(i - 2)!='0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i -1) - '0'))<=26){
+				dp[i] += dp[i-2];
+			}
+		}
+		return dp[length];
+	}
+
+}

afec0212fa6c00b04ff9291c41bfe6f85c00fc99
zhoujunjie <1367970137@qq.com>
2021-05-25 19:17:09 +0800

    最小路径-动态规划

---
+++ src/动态规划/普通/最小路径和.java
@@ -0,0 +1,19 @@
+package 动态规划.普通;
+
+// https://leetcode-cn.com/problems/minimum-path-sum/
+public class 最小路径和 {
+	public int minPathSum(int[][] grid) {
+		for (int i = 1; i < grid.length; i++) {
+			grid[i][0] += grid[i-1][0];
+		}
+		for (int i = 1; i < grid[0].length; i++) {
+			grid[0][i] += grid[0][i-1];
+		}
+		for (int i = 1; i < grid.length; i++) {
+			for (int j = 1; j < grid[0].length; j++) {
+				grid[i][j] += Math.min(grid[i-1][j] , grid[i][j-1]);
+			}
+		}
+		return grid[grid.length-1][grid[0].length-1];
+	}
+}

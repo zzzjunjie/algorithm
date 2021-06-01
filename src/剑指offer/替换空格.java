bc776de55e7419d6ba034e6891820843172e06f7
zhoujunjie <1367970137@qq.com>
2021-06-01 20:01:15 +0800

    替换空格

--- src/剑指offer/替换空格.java
+++ src/剑指offer/替换空格.java
@@ -1,24 +1,48 @@
 package 剑指offer;
 
+
 public class 替换空格 {
-    public String replaceSpace(StringBuffer  str) {
-        int P1 = str.length() - 1;
-        for (int i = 0; i <= P1; i++){
-            if (str.charAt(i) == ' '){
-                str.append("  ");
+
+	public String replaceSpace(String s) {
+		StringBuffer str = new StringBuffer(s);
+		int P1 = str.length() - 1;
+		for (int i = 0; i <= P1; i++) {
+			if (str.charAt(i) == ' ') {
+				str.append("  ");
+			}
+		}
+		int P2 = str.length() - 1;
+		while (P1 >= 0 && P2 > P1) {
+			char c = str.charAt(P1--);
+			if (c == ' ') {
+				str.setCharAt(P2--, '0');
+				str.setCharAt(P2--, '2');
+				str.setCharAt(P2--, '%');
+			} else {
+				str.setCharAt(P2--, c);
+			}
+		}
+		return str.toString();
+	}
+
+	public String replaceSpace2(String s) {
+		int length = s.length();
+		char[] chars = new char[length * 3];
+		int index = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ') {
+				chars[index++] = '%';
+				chars[index++] = '2';
+				chars[index++] = '0';
+			}else{
+			    chars[index++] = s.charAt(i);
             }
-        }
-        int P2 = str.length() - 1;
-        while (P1 >= 0 && P2 > P1) {
-            char c = str.charAt(P1--);
-            if (c == ' ') {
-                str.setCharAt(P2--, '0');
-                str.setCharAt(P2--, '2');
-                str.setCharAt(P2--, '%');
-            } else {
-                str.setCharAt(P2--, c);
-            }
-        }
-        return str.toString();
+		}
+		return new String(chars,0,index);
+	}
+
+    public static void main(String[] args) {
+        char[] chars = {'a','v',' '};
+        System.out.println(new String(chars,0,2));
     }
 }

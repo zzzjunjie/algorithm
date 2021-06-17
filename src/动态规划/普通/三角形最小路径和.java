591fd7c3b6464fe2e8ee116b871724c48ce9addc
zhoujunjie <1367970137@qq.com>
2021-06-18 00:29:11 +0800

    三角形最小路径和

--- src/动态规划/普通/三角形最小路径和.java
+++ src/动态规划/普通/三角形最小路径和.java
@@ -4,20 +4,21 @@ import java.util.List;
 
 public class 三角形最小路径和 {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] f = new int[n][n];
-        f[0][0] = triangle.get(0).get(0);
-        for (int i = 1; i < n; ++i) {
-            f[i][0] = f[i - 1][0] + triangle.get(i).get(0);
-            for (int j = 1; j < i; ++j) {
-                f[i][j] = Math.min(f[i - 1][j - 1], f[i - 1][j]) + triangle.get(i).get(j);
+        int size = triangle.size();
+        // 设dp[i][j]为三角形顶部到i,j点上的最小值
+        int[][] dp = new int[size][size];
+        dp[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < size; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle.get(i).get(j);
             }
-            f[i][i] = f[i - 1][i - 1] + triangle.get(i).get(i);
+            dp[i][i] = dp[i - 1][i - 1] + triangle.get(i).get(i);
         }
-        int minTotal = f[n - 1][0];
-        for (int i = 1; i < n; ++i) {
-            minTotal = Math.min(minTotal, f[n - 1][i]);
+        int min = dp[size - 1][0];
+        for (int i : dp[size - 1]) {
+            min = Math.min(min, i);
         }
-        return minTotal;
+        return min;
     }
 }

e8be93248061027a55fd2e79e2511ca6c688b549
zhoujunjie <1367970137@qq.com>
2021-07-14 23:56:36 +0800

    统计作战单位数

--- src/动态规划/普通/统计作战单位数.java
+++ src/动态规划/普通/统计作战单位数.java
@@ -20,7 +20,7 @@ public class 统计作战单位数 {
         return res;
     }
 
-    public int numTeams(int[] rating) {
+    public int numTeams2(int[] rating) {
         int res = 0;
         for (int i = 1; i < rating.length - 1; i++) {
             int leftDown = 0;
@@ -44,5 +44,39 @@ public class 统计作战单位数 {
             res += leftDown * rightUp + leftUp * rightDown;
         }
         return res;
+    }
+
+    public int numTeams3(int[] rating) {
+        /*
+        找多少个升序组合和多少个降序组合
+
+        up 记录从左到右升序， down 记录从左到右降序
+        up 和 down 记录 [0, i - 1] 有多少个 比 i 小的 和 比 i 大的
+
+        规定 j < i，我们固定 i 和 j，然后根据 rating[j] 和 rating[i] 的关系进行处理
+        如果 rating[j] > rating[i]，那么就相当于是降序，那么我们找 j 前面有多少个比 j 大的元素，这些元素每一个都能够跟 j 和 i 构成组合，即 c += down[j]
+        如果 rating[j] < rating[i]，那么就相当于是升序，那么我们找 j 前面有多少个比 j 小的元素，这些元素每一个都能够跟 j 和 i 构成组合，即 c += up[j]
+        如果 rating[j] == rating[i]，直接忽略
+
+        状态可以压缩的，这里方便理解就不整了
+        */
+        int len = rating.length;
+        int[] up = new int[len];
+        int[] down = new int[len];
+
+        int c = 0;
+        for(int i = 0; i < len; i++){
+            for(int j = i - 1; j >= 0; j--){
+                //比它大，找降序
+                if(rating[j] > rating[i]){
+                    c += down[j];
+                    down[i]++;
+                }else if(rating[j] < rating[i]){
+                    c += up[j];
+                    up[i]++;
+                }
+            }
+        }
+        return c;
     }
 }
